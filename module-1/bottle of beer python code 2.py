48fa5bc4a5841441e92428c6bb59130defb6d24f
Cybernerd360 <msiggers@my365.bellevue.edu>
2026-09-12 12:48:36 -0400

    bottles of beer python code

---
+++ module-1/bottle of beer python code 2.py
@@ -0,0 +1,17 @@
+def countdown(user_bottles):
+    # main function that takes the user input and counts down the number of bottles of beer on the wall.
+    while user_bottles >0:
+        current_bottles = user_bottles -1
+        print (f"{user_bottles} bottles of beer on the wall, {user_bottles} bottles of beer.")
+        print (f"Take one down and pass it around, {current_bottles} bottles of beer on the wall.")
+# While the number of bottles is greater than 0, the program will print the current number of bottles and decrement the count by 1.
+        user_bottles -= 1
+# bottle number is decremented by 1 each time the loop runs, and the program will continue to print the current number of bottles until it reaches 0.
+        if user_bottles == 0:
+            print ("0 bottles of beer on the wall, no more bottles of beer.")
+            print ("Go to the store and buy some more, 99 bottles of beer on the wall.")
+# If the user inputs 0 or number reaches 0, the program will print the final lines of the song and end the countdown.
+user_bottles = int(input("Enter the number of bottles of beer: "))
+# user inputs the number of bottles of beer, and the countdown function is called with that input.
+countdown(user_bottles)
+# The countdown function is called with the user input, and the program will continue to run until the number of bottles reaches 0. Each iteration of the loop will print the current number of bottles and decrement the count by 1.
